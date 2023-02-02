6c453d68150b1bfa2b4c62f559e9edeacad12cdf
lucas <gepeoverload@gmail.com>
2023-02-02 16:29:13 +0100

    decoupled tests

--- cypress/tests/ui/basicOperations.spec.ts
+++ cypress/tests/ui/basicOperations.spec.ts
@@ -22,9 +22,9 @@ describe("Basic CRUD operations", () => {
 
     it("should update an existing pet in the petStore by its id", () => {
         cy.intercept('PUT', `${apiUrl}`).as('updating-pet')
-        cy.updatePet()
 
         cy.fixture('updatePetData.json').then((fixture) => {
+            cy.updatePet()
             cy.wait('@updating-pet').then((interception) => {
                 expect(interception.response.body).to.deep.eq(fixture)
             })
@@ -37,9 +37,9 @@ describe("Basic CRUD operations", () => {
 
     it("should get an existing pet from the petStore by its id", () => {
         cy.intercept('GET', `${apiUrl}/*`).as('getting-pet')
-        cy.getPet()
-
         cy.fixture('updatePetData.json').then((fixture) => {
+            cy.request('POST', apiUrl, fixture)
+            cy.getPet()
             cy.wait('@getting-pet').then((interception) => {
                 expect(interception.response.body).to.deep.eq(fixture)
             })
@@ -52,9 +52,10 @@ describe("Basic CRUD operations", () => {
 
     it("should delete an existing pet from the petStore by its id", () => {
         cy.intercept('DELETE', `${apiUrl}/*`).as('deleting-pet')
-        cy.deletePet()
 
         cy.fixture('updatePetData.json').then((fixture) => {
+            cy.request('POST', apiUrl, fixture)
+            cy.deletePet()
             cy.wait('@deleting-pet').then((interception) => {
                 expect(interception.response.statusCode).to.equal(200)
                 expect(interception.response.body.message).to.contain(fixture.id)
